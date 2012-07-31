925d2856fb72f2d2054c611d62a9176405d7b87a
andrerpbts <andrerpbts@gmail.com>
2012-07-30 23:34:29 -0300

    Nomes dos relacionamentos ajustados para plural

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,8 +1,8 @@
 class Curso < ActiveRecord::Base
   has_many :curso_disciplina_periodos
 
-  has_many :disciplina, :through => :curso_disciplina_periodos
-  has_many :periodo, :through => :curso_disciplina_periodos
+  has_many :disciplinas, :through => :curso_disciplina_periodos
+  has_many :periodos, :through => :curso_disciplina_periodos
 
   attr_accessible :nome
 end
--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,8 +1,8 @@
 class Disciplina < ActiveRecord::Base
   has_many :curso_disciplina_periodos
 
-  has_many :periodo, :through => :curso_disciplina_periodos
-  has_many :curso, :through => :curso_disciplina_periodos
+  has_many :periodos, :through => :curso_disciplina_periodos
+  has_many :cursos, :through => :curso_disciplina_periodos
 
   attr_accessible :nome
 
